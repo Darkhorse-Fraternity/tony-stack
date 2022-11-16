38585098a06117b92226b4d86b89a3387610aed1
tonglin <tonglin@hireez.com>
2022-11-16 16:55:24 +0800

    chore: update config

--- packages/use-hook-utils/index.tsx
+++ packages/use-hook-utils/index.tsx
@@ -1,4 +1,4 @@
 export * from "./lib/use-debounce"
-export { useIsomorphicLayoutEffect } from "./lib/use-Isomorphic-layout-effect"
+export { useIsomorphicLayoutEffect } from "./lib/use-isomorphic-layout-effect"
 export * from "./lib/use-local-storage"
 export { usePrevious } from "./lib/use-previous"
