9dc302bb42a512054bd1885cd0a8de09ef3d9666
tonglin <tonglin@hireez.com>
2023-01-19 15:47:56 +0800

    chore: update ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,30 @@
 /** @type {import("eslint").Linter.Config} */
-module.exports = {
+const config = {
+  extends: ["prettier", "eslint:recommended"],
+  overrides: [
+    {
+      extends: ["monad/turbo"],
+      files: ["**/*.ts", "**/*.tsx"],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: [
+          "./tsconfig.json",
+          "./apps/*/tsconfig.json",
+          "./packages/*/tsconfig.json",
+        ],
+      },
+    },
+  ],
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-monad`
-  parser: "@typescript-eslint/parser",
-  extends: ["monad/turbo"],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: [
-      "./tsconfig.json",
-      "./apps/*/tsconfig.json",
-      "./packages/*/tsconfig.json",
-    ],
-  },
-}
+  reportUnusedDisableDirectives: true,
+  ignorePatterns: [
+    ".eslintrc.js",
+    "**/*.config.js",
+    "**/*.config.cjs",
+    "packages/config/**",
+  ],
+};
+
+module.exports = config;
+
+
