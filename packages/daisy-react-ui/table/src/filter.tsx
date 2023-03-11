fc943a57d4ae4f9144bc45d84d835e9dab050464
林通 <lintong320@gmail.com>
2023-03-12 00:05:48 +0800

    fix: table spinner

--- packages/daisy-react-ui/table/src/filter.tsx
+++ packages/daisy-react-ui/table/src/filter.tsx
@@ -40,8 +40,10 @@ export function DebouncedInput({
   )
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
   // Rank the item
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   const itemRank = rankItem(row.getValue(columnId), value)
 
   // Store the itemRank info
